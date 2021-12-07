5f6e16be3463aa1fa7c6f9defda6f4ba6880cbc1
Scott <13770321+scott-8@users.noreply.github.com>
2021-12-07 15:58:12 -0500

    Add files

---
+++ redis.py
@@ -0,0 +1,40 @@
+import requests
+import yaml
+
+r = requests.get("https://raw.githubusercontent.com/ScoopInstaller/Main/master/bucket/redis5.json")
+d = r.json()
+
+meta = {
+    "package": {
+        "name": "redis",
+        "version": d["version"],
+    },
+    "source": {
+        "url": d["architecture"]["64bit"]["url"],
+        "sha256": d["architecture"]["64bit"]["hash"],
+    },
+    "build": {
+        "skip": "True # [not win64]",
+        "script": [f"copy {i} %LIBRARY_BIN%\\{i}" for i in d["bin"]],
+    },
+    "test": {
+        "commands": [
+            "redis-server --version",
+            "redis-cli --version",
+        ],
+    },
+    "about": {
+        "home": d["homepage"],
+        "license": d["license"],
+        "summary": d["description"],
+        "dev_url": d["checkver"]["github"],
+    },
+    "extra": {
+        "maintainers": [
+            "scott",
+        ]
+    }
+}
+
+with open("meta.yaml", "w") as f:
+    yaml.dump(meta, f, sort_keys=False)
